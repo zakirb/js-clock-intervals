5319d9113f1f288f05e6d228fb334299967ec232
Zakir Butte <zakir.butte@gmailcom>
2018-01-25 20:47:02 -0800

    added current time starting point

--- clock.js
+++ clock.js
@@ -1,12 +1,32 @@
 
+//used stack overflow to find code block below since I don't understand the Date object or its uses
+
+var now = new Date(),
+    then = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        0,0,0);
+    var msSinceMidnight = now.getTime() - then.getTime(); // difference in millisecond
+//-------------------------------------------------------------
+
+var hourStartingDegrees = (msSinceMidnight % 43200000) / 120000;
+
+var minuteStartingDegrees = (msSinceMidnight % 3600000) / 10000;
+
+
+var secondStartingDegrees = (msSinceMidnight % 60000) * 6 / 1000;
+
+
 
 
 
 //hour
 
-var hourDegrees = 0;
+var hourDegrees = hourStartingDegrees;
 
 var hour = document.getElementById("hour");
+hour.style.transform = "rotate(" + hourDegrees + "deg)"; //used to set to current time without interval delay
 
 
 var hourTick = function() {
@@ -19,9 +39,10 @@ var hourTick = function() {
 
 //minute
 
-var minuteDegrees = 0;
+var minuteDegrees = minuteStartingDegrees;
 
 var minute = document.getElementById("minute");
+minute.style.transform = "rotate(" + minuteDegrees + "deg)"; //used to set to current time without interval delay
 
 
 var minuteTick = function() {
@@ -34,9 +55,10 @@ var minuteTick = function() {
 //seconds
 
 
-var secondDegrees = 0;
+var secondDegrees = secondStartingDegrees;
 
 var second = document.getElementById("second");
+second.style.transform = "rotate(" + secondDegrees + "deg)"; //used to set to current time without interval delay
 
 
 var secondTick = function () {
